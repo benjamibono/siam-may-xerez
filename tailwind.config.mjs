8027b0f28b944e99b2ba8fdd6516e98bda3dc4ff
benjamibono <benjamibono@gmail.com>
2024-05-22 19:51:21 +0200

    animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -11,5 +12,5 @@ export default {
 			xl: '1280px',
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
