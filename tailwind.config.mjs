ffebdc1caeb7e12c9377eb14bb0e5b9b23121d6a
benjamibono <benjamibono@gmail.com>
2025-03-26 16:34:32 +0100

    Actualizar configuración de Tailwind CSS para incluir nuevos breakpoints y mejorar la estructura de los componentes, además de ajustar estilos en varios componentes como Carousel, Footer, Galeria, Intro, NavBar y Selector para una mejor presentación y responsividad.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		screens: {
-			xs: '460px',
-			sm: '640px',
-			md: '768px',
-			lg: '1024px',
-			xl: '1280px',
-		},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "460px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "2160px",
+    },
+  },
+  plugins: [animations],
+};
